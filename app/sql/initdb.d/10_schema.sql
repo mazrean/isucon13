421c8691b84a9a50ffcc89460869343eb0d24024
mazrean <shunsuke.w04@gmail.com>
2023-11-25 16:31:51 +0900

    ngword index

--- app/sql/initdb.d/10_schema.sql
+++ app/sql/initdb.d/10_schema.sql
@@ -93,7 +93,8 @@ CREATE TABLE `ng_words` (
   `user_id` BIGINT NOT NULL,
   `livestream_id` BIGINT NOT NULL,
   `word` VARCHAR(255) NOT NULL,
-  `created_at` BIGINT NOT NULL
+  `created_at` BIGINT NOT NULL,
+  INDEX `ng_words_livestream_id` (`livestream_id`, `user_id`)
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 CREATE INDEX ng_words_word ON ng_words(`word`);
 
